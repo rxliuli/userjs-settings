dbd89426b2ad98f808d2addd4a005b5902554bd3
rxliuli <rxliuli@gmail.com>
2018-12-20 12:57:46 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer
+      fixed
+      app
+    >
+      <span>&copy; rxliuli 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import routes from '@/router/routes'
+
+export default {
+  data () {
+    return {
+      drawer: true,
+      items: [],
+      title: 'Vuetify.js'
+    }
+  },
+  methods: {
+    init () {
+      console.log(routes)
+      window.routes = routes
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  name: 'App'
+}
+</script>
