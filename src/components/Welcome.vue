b8c57a52fa741789e54699c29baafecbe146c4af
rxliuli <rxliuli@gmail.com>
2018-12-23 19:43:52 +0800

    更新 [首页] 的欢迎内容

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -16,7 +16,7 @@
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">user.js 配置</h3>
-              <div>这里是 rxliuli 写的 user.js 的配置网站，欢迎来到这儿进行配置！</div>
+              <div>这里是 rxliuli 的 user.js 配置网站，欢迎来到这儿进行设置已安装的脚本！</div>
             </div>
           </v-card-title>
 
@@ -37,16 +37,3 @@
     </v-layout>
   </v-container>
 </template>
-
-<script>
-export default {
-  data: () => ({
-
-  }),
-  methods: {
-  },
-  mounted () {
-
-  }
-}
-</script>
